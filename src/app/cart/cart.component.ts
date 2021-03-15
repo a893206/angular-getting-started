33e52b79ff6d2ef843ea16bbe6fbdc5208564956
CR <931009686@qq.com>
2021-03-15 12:57:07 +0800

    使用表单进行用户输入

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
-import { Product } from '../class/product';
+import {Product} from '../class/product';
 
 @Component({
   selector: 'app-cart',
@@ -9,13 +11,27 @@ import { Product } from '../class/product';
 })
 export class CartComponent implements OnInit {
   items: Array<Product> = [];
+  checkoutForm;
 
   constructor(
-      private cartService: CartService
-  ) { }
+      private cartService: CartService,
+      private formBuilder: FormBuilder
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
 
   ngOnInit(): void {
     this.items = this.cartService.getItems();
   }
 
+  onSubmit(customerData: {name: string, address: string}): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
 }
